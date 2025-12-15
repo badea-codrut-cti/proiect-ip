1852c3fff3f62c49bba7358e76d882961190010e
oreoezi <oreoezi@oreo.ac>
2025-12-15 22:48:04 +0100

    Fixed npm run dev

---
+++ backend/counter/index.ts
@@ -0,0 +1,26 @@
+import { ageCounterToKana } from './ageCounter';
+import { dateCounterToKana } from './dateCounter';
+import { digitEndingCounterToKana, digitEndingCounters } from './digitEndingCounter';
+
+const COUNTERS: Record<string, (counter: string, number: number | string) => string> = {
+  '歳': (_, number) => ageCounterToKana(number),
+  '才': (_, number) => ageCounterToKana(number),
+  '日': (_, number) => dateCounterToKana(number),
+};
+
+const DIGIT_ENDING_COUNTERS = Object.keys(digitEndingCounters);
+
+export function counterToKana(counter: string, number: number | string): string {
+  if (COUNTERS[counter]) {
+    return COUNTERS[counter](counter, number);
+  }
+
+  if (DIGIT_ENDING_COUNTERS.includes(counter)) {
+    return digitEndingCounterToKana(counter, number);
+  }
+
+  throw new Error(`Counter "${counter}" is not supported`);
+}
+
+export { ageCounterToKana, dateCounterToKana, digitEndingCounterToKana };
+
